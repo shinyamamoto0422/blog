9fbc7201129b986d56ca4540b2e20b30e68f6368
code-yy <jiyeyouren@gmail.com>
2022-05-25 21:04:50 +0900

    🔐 serviceDomainとapiKeyを環境変数化

--- src/config/api.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "microcms-js-sdk"; //ES6
-
-export const client = createClient({
-  serviceDomain: "code-yy-blog",
-  apiKey: "1aa6fab79f3b403a92071723efaffb0e583a",
-});
