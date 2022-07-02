d3a503920cd8531fe8b793decd0c4fbf8e15c9b4
code-yy <jiyeyouren523@gmail.com>
2022-07-02 10:30:19 +0900

    ♻️ geLayoutをちゃんと使えるように

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,17 +1,40 @@
-import { FC, ReactNode } from "react";
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+} from "@mantine/core";
+import Head from "next/head";
+import { useState } from "react";
+import { GetLayout } from "@/types/next-type";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
 
-type Props = {
-  children: ReactNode;
-};
+export const MainLayout: GetLayout = (page) => {
+  const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
+  const toggleColorScheme = (value?: ColorScheme) => {
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+  };
 
-export const MainLayout: FC<Props> = ({ children }) => {
   return (
     <>
-      <Header />
-      {children}
-      <Footer />
+      <Head>
+        <title>Code yy</title>
+        <link rel="icon" type="image/png" sizes="16x16" href="/god.png" />
+      </Head>
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
+      >
+        <MantineProvider
+          theme={{ colorScheme }}
+          withGlobalStyles
+          withNormalizeCSS
+        >
+          <Header />
+          <main>{page}</main>
+          <Footer />
+        </MantineProvider>
+      </ColorSchemeProvider>
     </>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,43 +1,10 @@
 import "../styles/globals.css";
-import Head from "next/head";
-import { FC, useState } from "react";
-import {
-  ColorScheme,
-  ColorSchemeProvider,
-  MantineProvider,
-} from "@mantine/core";
 import { AppPropsWithLayout } from "@/types/next-type";
-import { MainLayout } from "@/components/layout";
 
-const App: FC<AppPropsWithLayout> = ({ Component, pageProps }) => {
-  const Layout = Component.useLayout ? Component.useLayout : MainLayout;
-  const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
-  const toggleColorScheme = (value?: ColorScheme) => {
-    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
-  };
+const App = ({ Component, pageProps }: AppPropsWithLayout) => {
+  const Layout = Component.getLayout ?? ((page) => page);
 
-  return (
-    <>
-      <Head>
-        <title>Code yy</title>
-        <link rel="icon" type="image/png" sizes="16x16" href="/god.png" />
-      </Head>
-      <ColorSchemeProvider
-        colorScheme={colorScheme}
-        toggleColorScheme={toggleColorScheme}
-      >
-        <MantineProvider
-          theme={{ colorScheme }}
-          withGlobalStyles
-          withNormalizeCSS
-        >
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </MantineProvider>
-      </ColorSchemeProvider>
-    </>
-  );
+  return Layout(<Component {...pageProps} />);
 };
 
 export default App;
