22133e5f37828463a461fa67facb6ab49502023c
code-yy <jiyeyouren523@gmail.com>
2022-07-02 10:31:01 +0900

    🎨 トップページにブログを表示して、ある程度スタイリング

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,28 @@
-import { NextPageWithLayout } from "@/types/next-type";
 import { client } from "@/libs/microCMS/client";
 import { Home } from "@/components/page";
+import { Blogs } from "@/types/blog";
+import { NextPageWithLayout } from "@/types/next-type";
+import { MainLayout } from "@/components/layout";
+
+export type Props = {
+  blogs: Blogs;
+};
 
 export const getStaticProps = async () => {
-  const data = await client.get({
+  const blogs = await client.get({
     endpoint: "blogs",
   });
   return {
     props: {
-      data,
+      blogs,
     },
   };
 };
 
-const HomePage: NextPageWithLayout = ({ data }: any) => {
-  return <Home data={data} />;
+const HomePage: NextPageWithLayout<Props> = ({ blogs }) => {
+  return <Home blogs={blogs} />;
 };
+
+HomePage.getLayout = MainLayout;
 
 export default HomePage;
