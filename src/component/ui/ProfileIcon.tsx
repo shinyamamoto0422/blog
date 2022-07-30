1a9747ae0f88c5d033ae38ddd0481269ff496bb7
code-yy <jiyeyouren523@gmail.com>
2022-07-30 14:53:32 +0900

    ♻️

--- src/component/ui/ProfileIcon.tsx
+++ src/component/ui/ProfileIcon.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import { FC } from "react";
-import { godIcon } from "@/constant/src";
 
 export const ProfileIcon: FC = () => {
-  return <Image src={godIcon} alt={godIcon} width={44} height={44} className="rounded-full" />;
+  return <Image src={"/god.png"} alt={"/god.png"} width={44} height={44} className="rounded-full" />;
 };
